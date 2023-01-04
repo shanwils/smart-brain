97188637d94f934cdeb229d9dd00d4a737997ab6
shanwils <64047685+shanwils@users.noreply.github.com>
2023-01-04 10:57:01 -0500

    fixing image link

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://infinite-river-67054.herokuapp.com:3001/image', {
+          fetch('https://infinite-river-67054.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
